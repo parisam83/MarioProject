07af2cf9e466c9bfb374eadd897cd9d8ac0057b7
Parisa <mousaviparisa5@yahoo.com>
2023-04-05 12:37:38 +0330

    Create UserController.java

---
+++ src/main/java/com/parim/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.parim.controller;
+
+import com.parim.access.UserAccess;
+import com.parim.event.UserFormEvent;
+import com.parim.model.User;
+
+public class UserController {
+    private final UserFormEvent userFormEvent;
+    private final UserAccess userAccess = new UserAccess();
+
+    public  UserController(UserFormEvent userFormEvent){
+        this.userFormEvent = userFormEvent;
+    }
+
+    public User registerUser(){
+        if (!userAccess.isDuplicate(userFormEvent.getUsername())){
+            User user = new User(userAccess.numberOfUsers() + 1, userFormEvent.getUsername(), userFormEvent.getPassword());
+            userAccess.add(user);
+            return user;
+        }
+        return null;
+    }
+
+    public User loginUser(){
+        return userAccess.findUser(userFormEvent.getUsername(), userFormEvent.getPassword());
+    }
+}
