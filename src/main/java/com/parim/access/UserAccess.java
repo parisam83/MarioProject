1692cf548f437cd14e471ab3c7a176c91a216631
Parisa <mousaviparisa5@yahoo.com>
2023-04-05 12:36:46 +0330

    Create UserAccess.java

---
+++ src/main/java/com/parim/access/UserAccess.java
@@ -0,0 +1,56 @@
+package com.parim.access;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.parim.model.User;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class UserAccess {
+    private final String directory = "database";
+    private final File databaseFile = new File(directory);
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private ArrayList<User> users;
+
+    private void read(){
+        for (int i = 1; i <= numberOfUsers(); i++) {
+            FileReader reader;
+            try {
+                reader = new FileReader(directory + "/user" + String.valueOf(i) + ".json");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            users.add(gson.fromJson(reader, User.class));
+        }
+    }
+    public void add(User user){
+        try {
+            FileWriter writer = new FileWriter(directory + "/user" + user.getId() + ".json");
+            gson.toJson(user, writer);
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public boolean isDuplicate(String username){
+        read();
+        for (User user : users)
+            if (user.getUsername().equals(username))
+                return true;
+        return false;
+    }
+    public User findUser(String username, String password){
+        read();
+        for (User user : users)
+            if (user.getUsername().equals(username) && user.getPassword().equals(password))
+                return user;
+        return null;
+    }
+
+    public int numberOfUsers(){
+        if (databaseFile.list() == null)
+            return 0;
+        return databaseFile.list().length;
+    }
+}
