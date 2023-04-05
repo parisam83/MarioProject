b9853f51a2df55051dc341349857ecf0736269ef
Parisa <mousaviparisa5@yahoo.com>
2023-04-05 14:01:56 +0330

    Implement the connection between LoginListener and MainFrame

--- src/main/java/com/parim/listener/LoginListener.java
+++ src/main/java/com/parim/listener/LoginListener.java
@@ -2,10 +2,18 @@ package com.parim.listener;
 
 import com.parim.controller.UserController;
 import com.parim.event.UserFormEvent;
+import com.parim.model.User;
+import com.parim.view.MainFrame;
 
 public class LoginListener implements FormListener{
     @Override
     public void eventOccurred(UserFormEvent userFormEvent) {
-        UserController userController = new UserController(userFormEvent);
+        User user = new UserController(userFormEvent).loginUser();
+        if (user == null)
+            MainFrame.getInstance().unsuccessfulLoginError();
+        else{
+            MainFrame.setUser(user);
+            MainFrame.getInstance().setGamePage();
+        }
     }
 }
