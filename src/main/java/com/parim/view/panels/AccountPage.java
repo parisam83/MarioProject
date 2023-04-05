59daf4434af86ef66cb7acd24c73a3ee254e1c0d
Parisa <mousaviparisa5@yahoo.com>
2023-04-05 10:18:04 +0330

    Create abstract class AccountPage.java

---
+++ src/main/java/com/parim/view/panels/AccountPage.java
@@ -0,0 +1,41 @@
+package com.parim.view.panels;
+
+import com.parim.view.MainFrame;
+import com.parim.view.objects.swingObjects.ButtonCreator;
+import com.parim.view.objects.swingObjects.LabelCreator;
+import com.parim.view.objects.swingObjects.TextFieldCreator;
+
+import javax.swing.*;
+
+public abstract class AccountPage extends JPanel {
+    private static final int textFieldLabelGap = 230, textFieldGap = 180;
+    protected LabelCreator label;
+    protected TextFieldCreator username, password;
+    public AccountPage(){
+        setLabel();
+        this.add(label);
+
+        username = new TextFieldCreator(textFieldLabelGap, "username");
+        this.add(username);
+
+        password = new TextFieldCreator(textFieldLabelGap + textFieldGap, "password");
+        this.add(password);
+
+        ButtonCreator nextButton = new ButtonCreator(MainFrame.getGameWidth()/2 + 300, textFieldLabelGap + textFieldGap*7/3, ButtonCreator.getNextButtonWidth(), ButtonCreator.getNextButtonHeight(), ">> Next >>", true);
+        nextButton.addActionListener(e -> validInput());
+        this.add(nextButton);
+
+        ButtonCreator backButton = new ButtonCreator(MainFrame.getGameWidth()/2 - 300 - ButtonCreator.getNextButtonWidth(), textFieldLabelGap + textFieldGap*7/3, ButtonCreator.getNextButtonWidth(), ButtonCreator.getNextButtonHeight(), "<< Back <<", true);
+        backButton.addActionListener(e -> MainFrame.getInstance().setFirstPage());
+        this.add(backButton);
+
+        this.setLayout(null);
+        this.setPreferredSize(MainFrame.getScreenSize());
+    }
+
+    public boolean isDefaultValue(){
+        return username.getText().equals("username") && password.getText().equals("password");
+    }
+    public abstract void setLabel();
+    public abstract void validInput();
+}
