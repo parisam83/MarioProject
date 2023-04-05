15b4a370e9c18c9c30fa57a3f020630af1a21d38
Parisa <mousaviparisa5@yahoo.com>
2023-04-05 13:48:07 +0330

    Create GamePage.java

---
+++ src/main/java/com/parim/view/panels/GamePage.java
@@ -0,0 +1,12 @@
+package com.parim.view.panels;
+
+import com.parim.view.MainFrame;
+
+import javax.swing.*;
+
+public class GamePage extends JPanel {
+    public GamePage(){
+        this.setLayout(null);
+        this.setPreferredSize(MainFrame.getScreenSize());
+    }
+}
