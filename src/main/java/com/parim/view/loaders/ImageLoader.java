4f13f1a4d7ec08e6f1d5c1fc9752f2cd52cf818c
Parisa <mousaviparisa5@yahoo.com>
2023-04-07 23:39:04 +0330

    Add getSmall and getBig ImageIcon to ImageLoader.java

--- src/main/java/com/parim/view/loaders/ImageLoader.java
+++ src/main/java/com/parim/view/loaders/ImageLoader.java
@@ -8,11 +8,13 @@ import java.io.IOException;
 
 public class ImageLoader {
     private final Image picture;
-
+    private final ImageIcon small, big;
     public ImageLoader(String name){
         {
             try {
                 picture = ImageIO.read(new File("src/main/resources/characters/" + name));
+                big = new ImageIcon(picture.getScaledInstance(400, 400, Image.SCALE_SMOOTH));
+                small = new ImageIcon(picture.getScaledInstance(190, 190, Image.SCALE_SMOOTH));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -22,7 +24,10 @@ public class ImageLoader {
     public Image getPicture() {
         return picture;
     }
-    public ImageIcon getIcon(){
-        return new ImageIcon(picture);
+    public ImageIcon getSmall() {
+        return small;
+    }
+    public ImageIcon getBig() {
+        return big;
     }
 }
