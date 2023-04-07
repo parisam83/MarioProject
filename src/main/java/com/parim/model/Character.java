4dccc02c89533ad12b67fae5ace3128a1ba3ede2
Parisa <mousaviparisa5@yahoo.com>
2023-04-07 22:34:19 +0330

    Add price and image to Character.java enum!

--- src/main/java/com/parim/model/Character.java
+++ src/main/java/com/parim/model/Character.java
@@ -1,9 +1,29 @@
 package com.parim.model;
 
+import com.parim.view.loaders.ImageLoader;
+
+import javax.swing.*;
+
 public enum Character {
-    Xayyat,
-    Frog, // jump
-    MigMig, // fast
-    Woman, // coin
-    RobinHood // shot
+    Xayyat(0, new ImageLoader("Xayyat.png").getIcon()),
+    Frog(100, new ImageLoader("Frog.png").getIcon()), // jump
+    MigMig(200, new ImageLoader("MigMig.png").getIcon()), // fast
+    Woman(300, new ImageLoader("Woman.png").getIcon()), // coin
+    RobinHood(400, new ImageLoader("RobinHood.png").getIcon()) // shot
+    ;
+
+    private final int price;
+    private final ImageIcon imageIcon;
+    Character(int price, ImageIcon imageIcon){
+        this.price = price;
+        this.imageIcon = imageIcon;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public ImageIcon getImageIcon() {
+        return imageIcon;
+    }
 }
