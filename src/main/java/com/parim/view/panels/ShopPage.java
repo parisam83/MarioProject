847c5f0f4e6adaa07d4be747feae9ed93de00451
Parisa <mousaviparisa5@yahoo.com>
2023-04-08 11:12:54 +0330

    Add action listener for shop buttons and swap current character with previous one graphically

--- src/main/java/com/parim/view/panels/ShopPage.java
+++ src/main/java/com/parim/view/panels/ShopPage.java
@@ -1,45 +1,57 @@
 package com.parim.view.panels;
 
+import com.parim.event.ShopEvent;
+import com.parim.listener.ShopListener;
 import com.parim.model.Character;
 import com.parim.model.User;
 import com.parim.view.MainFrame;
+import com.parim.view.loaders.FontLoader;
 import com.parim.view.objects.swingObjects.ButtonCreator;
 import com.parim.view.objects.swingObjects.LabelCreator;
+import com.parim.view.objects.swingObjects.ShopButtonCreator;
 
 import javax.swing.*;
 
 public class ShopPage extends JPanel {
-    private static final int bigImageSize = 500, smallImageSize = 250;
     private final User user;
+    private LabelCreator coins;
+    private ShopButtonCreator[] shopButtons = new ShopButtonCreator[5];
     public ShopPage(User user){
         this.user = user;
 
         LabelCreator label = new LabelCreator("Shop!");
         this.add(label);
 
-        ButtonCreator currentCharacterButton = new ButtonCreator(MainFrame.getGameWidth()/2 - bigImageSize - 100, MainFrame.getGameHeight()/2 - bigImageSize/2, bigImageSize, bigImageSize, String.valueOf(user.getCurrentCharacter().getPrice()) + " coins :: bought", false, user.getCurrentCharacter().getPicture().getBig());
-        this.add(currentCharacterButton);
-
-        int index = 0;
-        for (Character character : Character.values())
-            if (character != user.getCurrentCharacter()){
-                String text = " :: bought";
-                if (!user.getCharacters().contains(character)) text = "";
-
-                int x = MainFrame.getGameWidth()/2 + 100, y = MainFrame.getGameHeight()/2 - smallImageSize;
-                if (index > 1) x += smallImageSize;
-                if (index%2 == 0) y += smallImageSize;
-                index++;
-                ButtonCreator characterButton = new ButtonCreator(x, y, smallImageSize, smallImageSize, String.valueOf(character.getPrice()) + " coins" + text, false, character.getPicture().getSmall());
-                this.add(characterButton);
-            }
-
         ButtonCreator backButton = new ButtonCreator(MainFrame.getGameWidth()/2 - ButtonCreator.getNextButtonWidth()/2, 290 + 180*7/3, ButtonCreator.getNextButtonWidth(), ButtonCreator.getNextButtonHeight(), "<< Back <<", true);
         backButton.addActionListener(e -> MainFrame.getInstance().setGamePage());
         this.add(backButton);
 
+        coins = new LabelCreator(5, 10, "Coins: " + user.getCoins(), FontLoader.font.deriveFont(35f));
+        this.add(coins);
+
+        shopButtons[0] = new ShopButtonCreator(0, true, user.getCurrentCharacter());
+        this.add(shopButtons[0]);
+
+        int index = 1;
+        for (Character character : Character.values())
+            if (character != user.getCurrentCharacter()){
+                shopButtons[index] = new ShopButtonCreator(index, user.getCharacters().contains(character), character);
+                int finalIndex = index;
+                shopButtons[index].addActionListener(e -> setButtons(finalIndex, new ShopListener().eventOccurred(new ShopEvent(user, shopButtons[finalIndex].getCharacter()))));
+                this.add(shopButtons[index]);
+                index++;
+            }
+
         this.setLayout(null);
         this.setPreferredSize(MainFrame.getScreenSize());
+    }
+
+    public void setButtons(int smallIndex, String message){
+        coins.setText("Coins: " + user.getCoins());
+        if (!message.contains("successfully")) return;
+        System.out.println(shopButtons[0].getCharacter().name() + "   " + shopButtons[smallIndex].getCharacter().name());
+        System.out.println(shopButtons[0].getActionListeners().length + "   " + shopButtons[smallIndex].getActionListeners().length);
+        ShopButtonCreator.swapButtons(shopButtons[0], shopButtons[smallIndex]);
     }
 
     public User getUser() {
