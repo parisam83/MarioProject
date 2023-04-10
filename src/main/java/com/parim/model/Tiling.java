d6b13a20b98902e71ae4c262ac1b046133b3a392
Parisa <mousaviparisa5@yahoo.com>
2023-04-10 13:46:23 +0330

    Create Tiling model

---
+++ src/main/java/com/parim/model/Tiling.java
@@ -0,0 +1,13 @@
+package com.parim.model;
+
+public class Tiling {
+    private static final int objectSize = 60, marioSize = 80;
+
+    public static int getObjectSize() {
+        return objectSize;
+    }
+
+    public static int getMarioSize() {
+        return marioSize;
+    }
+}
