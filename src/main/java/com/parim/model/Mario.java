ee5a321d3717725959940b11b3f1488ce5d1e121
Parisa <mousaviparisa5@yahoo.com>
2023-04-15 00:06:42 +0330

    Fix mario move function

--- src/main/java/com/parim/model/Mario.java
+++ src/main/java/com/parim/model/Mario.java
@@ -1,40 +1,33 @@
 package com.parim.model;
 
 public class Mario {
-    private int x, y, firstY;
+    transient private Game game;
+    private int x, y, firstY, xDraw;
     private static int xVelocity, yVelocity;
     private static final int speed = 8, diff = 240+20;
     private static final int size = 80;
     private final String directory;
     private final Character character;
     private boolean gravity = false, jump = false, intersectDown = true;
-    public Mario(int x, int y, Character character){
-        this.x = x;
+    public Mario(int x, int y, Character character, Game game){
+        this.x = xDraw = x;
         this.y = y;
-        //area = new Area(x, y, size, size);
+        this.game = game;
         this.character = character;
         this.directory = "/characters/" + character.name() + ".png";
     }
 
     public void move(){
-        Scene scene = Scene.getInstance();
-
-        //if (gravity) yVelocity = speed;
-        //if (jump) yVelocity = -speed;
-
-        if (scene.intersectDown() && yVelocity > 0) {
+        if (game.intersectDown() && yVelocity > 0) {
             gravity = false;
             jump = false;
             yVelocity = 0;
         }
-        if (scene.intersectRight() && xVelocity > 0){
+        if (game.intersectRight() && xVelocity > 0){
             xVelocity = 0;
         }
 
-        //System.out.println(intersectDown);
-        //System.out.println(scene.intersectDown());
-        if (intersectDown && !scene.intersectDown()){
-            //System.out.println("biu paeiiiin");
+        if (intersectDown && !game.intersectDown()){
             intersectDown = false;
             if (!jump) {
                 gravity = true;
@@ -45,32 +38,19 @@ public class Mario {
         if (gravity && !jump && !intersectDown)
             yVelocity = speed;
 
-        if ((xVelocity > 0 && scene.intersectRight()) || (xVelocity < 0 && scene.intersectLeft()))
+        if ((xVelocity > 0 && game.intersectRight()) || (xVelocity < 0 && game.intersectLeft()))
             xVelocity = 0;
-        if (yVelocity > 0 && scene.intersectDown()) {
-            //y -= yVelocity*10;
+        if (yVelocity > 0 && game.intersectDown()) {
             yVelocity = 0;
             intersectDown = true;
             gravity = false;
         }
-        if (scene.intersectUp()) {
+        if (game.intersectUp()) {
             jump = false;
             gravity = true;
         }
 
-        //if (scene.intersectDown())
-        //System.out.println("intersectDown: " + intersectDown);
-
-        intersectDown = scene.intersectDown();
-        /*
-        System.out.println("gravity: " + gravity);
-        System.out.println("jump: " + jump);
-
-        System.out.println("intersectUp: " + scene.intersectUp());
-        System.out.println("intersectDown: " + scene.intersectDown());
-        System.out.println("intersectRight: " + scene.intersectRight());
-        System.out.println("intersectLeft: " + scene.intersectLeft() + "\n");
-*/
+        intersectDown = game.intersectDown();
         if (!jump && !gravity) yVelocity = 0;
         if (jump) yVelocity = -Math.abs(yVelocity);
         if (gravity) yVelocity = Math.abs(yVelocity);
@@ -80,30 +60,7 @@ public class Mario {
         }
         x += xVelocity;
         y += yVelocity;
-
-
-        /*if (scene.intersectRight()) {
-            //x -= xVelocity;
-            xVelocity *= -1;
-            //return true;
-        }
-*/
-
-        //scene.intersectRight();
-
-        //if (scene.intersectLeft())
-        //    x += xVelocity;
-
-        /*if (scene.intersectDown()) {
-            intersectDown = true;
-            gravity = false;
-            jump = false;
-        }*/
-        /*
-        if (scene.intersectDown())
-            yVelocity = 0;
-        if (scene.intersectRight() || scene.intersectLeft())
-            xVelocity = 0;*/
+        game.checkMarioLocation();
     }
 
     public static int getSize() {
@@ -153,5 +110,9 @@ public class Mario {
 
     public Character getCharacter() {
         return character;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
     }
 }
