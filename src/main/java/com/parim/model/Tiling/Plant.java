dcd63d18d8212709d7b276070c09e61ab3b04ee5
Parisa <mousaviparisa5@yahoo.com>
2023-04-15 02:05:45 +0330

    Modify move method in Plant.java to avoid change in Y coordinate

--- src/main/java/com/parim/model/Tiling/Plant.java
+++ src/main/java/com/parim/model/Tiling/Plant.java
@@ -1,8 +1,7 @@
 package com.parim.model.Tiling;
 
-import com.parim.model.Time;
-
 public class Plant extends Enemy{
+    private int numberOfCalls = 0;
     public Plant(int x, int y) {
         super(x, y, "/objects/Plant.png");
     }
@@ -11,7 +10,8 @@ public class Plant extends Enemy{
     @Override
     public void move() {
         super.move();
-        if ((Time.getSec() / 2) % 2 == 0)
+        numberOfCalls++;
+        if (((numberOfCalls/60) / 2) % 2 == 0)
             y++;
         else
             y--;
