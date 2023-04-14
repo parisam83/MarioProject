45a86a106866a5293d52135c418f5a663aef7290
Parisa <mousaviparisa5@yahoo.com>
2023-04-14 23:59:20 +0330

    Create Coin.java model

---
+++ src/main/java/com/parim/model/Tiling/Coin.java
@@ -0,0 +1,16 @@
+package com.parim.model.Tiling;
+
+public class Coin extends Tile{
+    private boolean visible = true;
+    public Coin(int x, int y) {
+        super(x, y, 45, 45, "/objects/Coin.png");
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+}
