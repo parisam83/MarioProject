7287f40aeafa765d59e6b017e52d5d9d60193af3
Parisa <mousaviparisa5@yahoo.com>
2023-04-15 00:09:11 +0330

    Add coinPowerUp boolean to PowerUpBlock.java

--- src/main/java/com/parim/model/Tiling/PowerUpBlock.java
+++ src/main/java/com/parim/model/Tiling/PowerUpBlock.java
@@ -1,7 +1,16 @@
 package com.parim.model.Tiling;
 
 public class PowerUpBlock extends Block{
+    private boolean coinPowerUp = true;
     public PowerUpBlock(int x, int y) {
         super(x, y, "/objects/PowerUpBlock.png");
+    }
+
+    public boolean isCoinPowerUp() {
+        return coinPowerUp;
+    }
+
+    public void setCoinPowerUp(boolean coinPowerUp) {
+        this.coinPowerUp = coinPowerUp;
     }
 }
