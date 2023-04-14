5bd2ae227f4b4fe4ba874c9c08c48bfa657be876
Parisa <mousaviparisa5@yahoo.com>
2023-04-15 00:07:05 +0330

    Override move function for Plant.java

--- src/main/java/com/parim/model/Tiling/Plant.java
+++ src/main/java/com/parim/model/Tiling/Plant.java
@@ -1,7 +1,19 @@
 package com.parim.model.Tiling;
 
+import com.parim.model.Time;
+
 public class Plant extends Enemy{
     public Plant(int x, int y) {
         super(x, y, "/objects/Plant.png");
+    }
+
+
+    @Override
+    public void move() {
+        super.move();
+        if ((Time.getSec() / 2) % 2 == 0)
+            y++;
+        else
+            y--;
     }
 }
