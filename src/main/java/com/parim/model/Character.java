40fbe1712514dbab9ee0bbb2edb0121e282a18c9
Parisa <mousaviparisa5@yahoo.com>
2023-04-15 19:59:35 +0330

    Change price of characters

--- src/main/java/com/parim/model/Character.java
+++ src/main/java/com/parim/model/Character.java
@@ -4,10 +4,10 @@ import com.parim.view.loaders.ImageLoader;
 
 public enum Character {
     Xayyat(0, new ImageLoader("characters/Xayyat.png")),
-    Frog(90, new ImageLoader("characters/Frog.png")), // jump
-    MigMig(230, new ImageLoader("characters/MigMig.png")), // fast
-    Woman(750, new ImageLoader("characters/Woman.png")), // coin
-    RobinHood(410, new ImageLoader("characters/RobinHood.png")) // shot
+    Frog(20, new ImageLoader("characters/Frog.png")), // jump
+    MigMig(37, new ImageLoader("characters/MigMig.png")), // fast
+    Woman(123, new ImageLoader("characters/Woman.png")), // coin
+    RobinHood(61, new ImageLoader("characters/RobinHood.png")) // shot
     ;
 
     private final int price;
