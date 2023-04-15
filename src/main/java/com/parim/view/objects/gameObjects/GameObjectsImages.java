28874a374a8928d299680ce288bcb4263ad9849c
Parisa <mousaviparisa5@yahoo.com>
2023-04-15 19:59:16 +0330

    Fix the bug of not changing the image of Mario

--- src/main/java/com/parim/view/objects/gameObjects/GameObjectsImages.java
+++ src/main/java/com/parim/view/objects/gameObjects/GameObjectsImages.java
@@ -1,6 +1,7 @@
 package com.parim.view.objects.gameObjects;
 
 
+import com.parim.model.Character;
 import com.parim.model.Mario;
 import com.parim.model.Tiling.Tile;
 import com.parim.view.loaders.ImageLoader;
@@ -11,6 +12,7 @@ import java.util.HashMap;
 public class GameObjectsImages {
     private static HashMap<String, Image> images = new HashMap<>();
     private static Image marioImage;
+    private static Character character;
     public static Image getImage(Tile tile){
         String directory = tile.getDirectory();
         if (images.get(directory) == null)
@@ -18,7 +20,8 @@ public class GameObjectsImages {
         return images.get(directory);
     }
     public static Image getImage(Mario mario){
-        if (marioImage == null) marioImage = mario.getCharacter().getPicture().getScaledPicture(Mario.getSize(), Mario.getSize());
+        if (character != mario.getCharacter()) marioImage = mario.getCharacter().getPicture().getScaledPicture(Mario.getSize(), Mario.getSize());
+        character = mario.getCharacter();
         return marioImage;
     }
 }
